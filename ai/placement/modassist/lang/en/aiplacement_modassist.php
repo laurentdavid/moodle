dc25595ee57ad6616d19904c423fb32852e6efdd
Laurent David <lmedavid@gmail.com>
2024-10-21 13:26:55 +0200

    MDL-83503 ai_modassist: New AI Module assistant
    * Add a new AI placement so modules (like glossary) can add custom actions
    * Add tests

---
+++ ai/placement/modassist/lang/en/aiplacement_modassist.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component aiplacement_modassist, language 'en'.
+ *
+ * @package    aiplacement_modassist
+ * @copyright  2024 Laurent David <laurent.david@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['apply'] = 'Apply';
+$string['drawer:intro'] = 'In this area you will see the results of the AI generation of the content you requested.';
+$string['generatefailtitle'] = 'Something went wrong';
+$string['generating'] = 'Generating your response';
+$string['nomodassist'] = 'AI Mod assistance is not available for this context';
+$string['pluginname'] = 'Module Assistance Placement';
+$string['privacy:metadata'] = 'The Module Assistance placement plugin does not store any personal data.';
+$string['regenerate'] = 'Regenerate';
+$string['results'] = 'Results';
+$string['tryagain'] = 'Try again';
